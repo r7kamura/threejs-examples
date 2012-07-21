ff342719d8ddd32604f495ce695273a85864e83e
Ryo NAKAMURA <ryo-nakamura@cookpad.com>
2012-07-22 01:23:30 +0900

    Add an example of first person

---
+++ first_person/js/firstPerson.js
@@ -0,0 +1,131 @@
+/*
+* @params
+*   camera    => THREE.camera
+*   moveSpeed => the scale of movement speed
+*
+* @feature
+*   click Left  => move forward
+*   click Right => move backward
+*   Key Left    => rotate laterally
+*   Key Right   => rotate laterally
+*   Key Up      => turn up
+*   Key Down    => turn down
+* */
+FirstPerson = function(args) {
+  // initialize
+
+  this.camera    = args.camera;
+  this.moveSpeed = args.moveSpeed || 1000;
+
+  this.canvas       = document;
+  this.viewHalfX    = window.innerWidth  / 2;
+  this.viewHalfY    = window.innerHeight / 2;
+  this.moveForward  = false;
+  this.moveBackward = false;
+  this.mouseX       = 0;
+  this.mouseY       = 0;
+  this.latitude     = 0;
+  this.longitude    = 0;
+  this.lookSpeed    = 0.01;
+  this.viewTarget   = new THREE.Vector3(0, 0, 100);
+
+  // public
+
+  this.update = function(delta) {
+    var moveDelta = this.moveSpeed * delta;
+
+    if (this.moveForward)  this.camera.translateZ(-moveDelta);
+    if (this.moveBackward) this.camera.translateZ( moveDelta);
+    if (this.moveLeft)     this.camera.translateX(-moveDelta);
+    if (this.moveRight)    this.camera.translateX( moveDelta);
+
+    var lookDelta = delta * this.lookSpeed;
+    this.longitude += this.mouseX * lookDelta;
+    this.latitude  -= this.mouseY * lookDelta;
+    this.latitude  = Math.max(-85, Math.min(85, this.latitude));
+
+    var latitudeRad  = this.latitude  * Math.PI / 180;
+    var longitudeRad = this.longitude * Math.PI / 180;
+    this.viewTarget.x = this.camera.position.x + 1000 * Math.cos(latitudeRad) * Math.cos(longitudeRad);
+    this.viewTarget.z = this.camera.position.z + 1000 * Math.cos(latitudeRad) * Math.sin(longitudeRad);
+    this.viewTarget.y = this.camera.position.y + 1000 * Math.sin(latitudeRad);
+    this.camera.lookAt({
+      x: this.viewTarget.x,
+      y: this.viewTarget.y,
+      z: this.viewTarget.z
+    });
+  }
+
+  // private
+
+  this.onMouseDown = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.mouseDrag = true;
+    switch(event.button) {
+      case 0: this.moveForward  = true; break;
+      case 2: this.moveBackward = true; break;
+    }
+  }
+
+  this.onMouseUp = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.mouseDrag = false;
+    switch(event.button) {
+      case 0: this.moveForward  = false; break;
+      case 2: this.moveBackward = false; break;
+    }
+  }
+
+  this.onMouseMove = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.mouseX = event.pageX - this.viewHalfX;
+    this.mouseY = event.pageY - this.viewHalfY;
+  }
+
+  this.onKeyDown = function(event) {
+    switch(keyString(event)) {
+      case 'Up':    this.moveForward  = true; break;
+      case 'Down':  this.moveBackward = true; break;
+      case 'Left':  this.moveLeft     = true; break;
+      case 'Right': this.moveRight    = true; break;
+    }
+  }
+
+  this.onKeyUp = function(event) {
+    switch(keyString(event)) {
+      case 'Up':    this.moveForward  = false; break;
+      case 'Down':  this.moveBackward = false; break;
+      case 'Left':  this.moveLeft     = false; break;
+      case 'Right': this.moveRight    = false; break;
+    }
+  }
+
+  this.onContextMenu = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    // Do nothing
+  }
+
+  this.bindKeyAndMouseEvents = function() {
+    this.canvas.addEventListener('contextmenu', bind(this, this.onContextMenu), false);
+    this.canvas.addEventListener('mousemove',   bind(this, this.onMouseMove),   false);
+    this.canvas.addEventListener('mousedown',   bind(this, this.onMouseDown),   false);
+    this.canvas.addEventListener('mouseup',     bind(this, this.onMouseUp),     false);
+    this.canvas.addEventListener('keyup',       bind(this, this.onKeyUp),       false);
+    this.canvas.addEventListener('keydown',     bind(this, this.onKeyDown),     false);
+
+    function bind(scope, fn) {
+      return function() {
+        fn.apply(scope, arguments);
+      };
+    };
+  }
+
+  this.bindKeyAndMouseEvents();
+};
---
+++ first_person/js/main.js
@@ -0,0 +1,140 @@
+$(function() {
+  var width,
+      height,
+      renderer,
+      camera,
+      scene,
+      light,
+      controls,
+      clock,
+      isMouseDown,
+      x     = 0,
+      y     = 0,
+      fps   = 0,
+      frame = 0,
+      sec   = 0;
+
+  init();
+
+  function initCanvasSize() {
+    var canvas = $('#canvas');
+    width  = canvas.width();
+    height = canvas.height();
+  }
+
+  function initRenderer() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(width, height);
+    renderer.setClearColorHex(0xFFFFFF, 1.0);
+  }
+
+  function initCamera() {
+    camera = new THREE.PerspectiveCamera(50, width / height, 1, 10000);
+    camera.position.x = -1000;
+    camera.position.y = 100;
+    camera.position.z = 0;
+    camera.up.x = 0;
+    camera.up.y = 1;
+    camera.up.z = 0;
+    camera.lookAt({
+      x: 0,
+      y: 100,
+      z: 0
+    });
+    scene.add(camera);
+  }
+
+  function initScene() {
+    scene = new THREE.Scene();
+  }
+
+  function initLight() {
+    light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+    light.position.set(0, 1000, 0);
+    scene.add(light);
+  }
+
+  function startLoop() {
+    loopPerFrame();
+    loopPerSec();
+  }
+
+  function initFloor() {
+    var sideNum = 10,
+        size    = 100,
+        tile;
+
+    for (var x = -sideNum; x < sideNum; x++) {
+      for (var z = -sideNum; z < sideNum; z++) {
+        tile = new THREE.Mesh(
+          new THREE.PlaneGeometry(size, size, 1, 1),
+          new THREE.MeshLambertMaterial({
+            color: ((x + z) % 2 == 0) ? 0x666666 : 0xDDDDDD
+          })
+        )
+        tile.position.set(
+          x * size,
+          0,
+          z * size
+        );
+        scene.add(tile);
+      }
+    }
+  }
+
+  function initControl() {
+    controls = new FirstPerson({
+      camera: camera,
+    });
+    clock = new THREE.Clock();
+  }
+
+  function loopPerFrame() {
+    frame++;
+    fps++;
+
+    controls.update(clock.getDelta());
+    updateStatusFrame();
+
+    renderer.clear();
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(loopPerFrame);
+  }
+
+  function loopPerSec() {
+    setInterval(function() {
+      updateStatusSec();
+      updateStatusFps();
+      fps = 0;
+      sec++;
+    }, 1000);
+  }
+
+  function updateStatusFps() {
+    $('#status .fps .value').text(fps);
+  }
+
+  function updateStatusSec() {
+    $('#status .sec .value').text(sec);
+  }
+
+  function updateStatusFrame() {
+    $('#status .frame .value').text(frame);
+  }
+
+  function render() {
+    $('#canvas').append(renderer.domElement);
+  }
+
+  function init() {
+    initCanvasSize();
+    initRenderer();
+    initScene();
+    initCamera();
+    initLight();
+    initFloor();
+    initControl();
+    startLoop();
+    render();
+  }
+});
